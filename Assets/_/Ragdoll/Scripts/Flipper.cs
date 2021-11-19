7e61a5ed44d2c180559b76f13d78e313e1cabe62
Willie <m4ds31@gmail.com>
2021-11-19 09:18:46 +0330

    backflip

--- Assets/_/Ragdoll/Scripts/Flipper.cs
+++ Assets/_/Ragdoll/Scripts/Flipper.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using PT.Utils;
 
 namespace DB.HeelFlip.Ragdoll
 {
@@ -10,20 +12,54 @@ namespace DB.HeelFlip.Ragdoll
         public void AddJoint()
         {
             _hipJoint = _pelvisRB.gameObject.AddComponent<ConfigurableJoint>();
-            _hipJoint.angularXDrive = hipS0;
-            _hipJoint.angularYZDrive = hipS1;
+            _hipJoint.slerpDrive = hipS0;
         }
 
+        public void RemovePelvisJoint()
+        {
+            try
+            {
+                hipS0 = _hipJoint.slerpDrive;
+                Destroy(_hipJoint);
+                foreach(ConfigurableJoint cj in GetComponentsInChildren<ConfigurableJoint>())
+                {
+                    cj.angularXDrive = new JointDrive();
+                    cj.angularYZDrive = new JointDrive();
+                }
+            }
+            catch { }
+        }
+
+        public void StickFoot()
+        {
+            if (!_footLock) { 
+                _rightFoot = _rightFootRB.gameObject.AddComponent<ConfigurableJoint>();
+                LockJoint(_rightFoot);
+                _leftFoot = _leftFootRB.gameObject.AddComponent<ConfigurableJoint>();
+                LockJoint(_leftFoot);
+                _footLock = true;
+            }
+        }
+
+        [FoldoutGroup("Joints")]
         [SerializeField] private ConfigurableJoint _leftKnee, _rightKnee, _leftHip, _rightHip;
+        [FoldoutGroup("Joints")]
         [SerializeField] private ConfigurableJoint _leftFoot, _rightFoot;
+        [FoldoutGroup("Joints")]
         [SerializeField] private ConfigurableJoint _hipJoint;
+
+        [FoldoutGroup("targetRotations")]
         [SerializeField] private Vector4 _leftHipGoal0, _rightHipGoal0;
+        [FoldoutGroup("targetRotations")]
         [SerializeField] private Vector4 _leftKneeGoal0, _rightKneeGoal0;
-        [SerializeField] private Rigidbody _pelvisRB;
+
+        [SerializeField] private Rigidbody _pelvisRB, _rightFootRB, _leftFootRB;
+        [SerializeField] private float _rotSpeed = 7, _stickyFeetDelay = 1, _hipsJointPower = 1000;
+        [SerializeField] private bool _footLock = true;
+        [SerializeField] private Vector3 _jump;
         [SerializeField] private Vector3 _angularVelocity;
 
         private bool _isClinched = false, _pressed = false;
-        private float _rot = 0;
 
         private JointDrive hipS0, hipS1;
 
@@ -35,7 +71,18 @@ namespace DB.HeelFlip.Ragdoll
             }
             if(!_isClinched && _pressed)
             {
-                
+                /*_hipJoint.targetRotation = 
+                    Quaternion.AngleAxis(Time.deltaTime * _rotSpeed, Vector3.forward) *
+                    _hipJoint.targetRotation;*/
+                _pelvisRB.angularVelocity = _angularVelocity;
+                //_pelvisRB.AddTorque(_angularVelocity, ForceMode.Impulse);
+                /*_pelvisRB.MoveRotation(
+                    Quaternion.AngleAxis(_rotSpeed * Time.deltaTime, Vector3.forward) *
+                    _pelvisRB.rotation
+                );*/
+                /*_hipJoint.targetRotation = 
+                    Quaternion.AngleAxis(_rotSpeed * Time.deltaTime, Vector3.forward) *
+                    _hipJoint.targetRotation;*/
             }
         }
 
@@ -76,12 +123,23 @@ namespace DB.HeelFlip.Ragdoll
             else
             {
                 _isClinched = false;
-                Destroy(_leftFoot);
-                Destroy(_rightFoot);
+                try
+                {
+                    Destroy(_rightFoot);
+                    Destroy(_leftFoot);
+                }
+                catch { }
 
-                hipS0 = _hipJoint.angularXDrive;
-                hipS1 = _hipJoint.angularYZDrive;
-                Destroy(_hipJoint);
+                TimeManager.Instance.DoWithDelay(_stickyFeetDelay, () =>
+                {
+                    _footLock = false;
+                });
+
+                _pelvisRB.velocity = _jump;
+                JointDrive jd = new JointDrive();
+                jd.maximumForce = 1000000;
+                jd.positionSpring = _hipsJointPower;
+                _hipJoint.slerpDrive = jd;
 
                 _leftHip.targetRotation = new Quaternion(
                     0,
@@ -111,6 +169,26 @@ namespace DB.HeelFlip.Ragdoll
                     0
                 );
             }
+        }
+
+        private void FreeUpJoint(ConfigurableJoint cj)
+        {
+            cj.xMotion = ConfigurableJointMotion.Free;
+            cj.yMotion = ConfigurableJointMotion.Free;
+            cj.zMotion = ConfigurableJointMotion.Free;
+            cj.angularXMotion = ConfigurableJointMotion.Free;
+            cj.angularYMotion = ConfigurableJointMotion.Free;
+            cj.angularZMotion = ConfigurableJointMotion.Free;
+        }
+
+        private void LockJoint(ConfigurableJoint cj)
+        {
+            cj.xMotion = ConfigurableJointMotion.Locked;
+            cj.yMotion = ConfigurableJointMotion.Locked;
+            cj.zMotion = ConfigurableJointMotion.Locked;
+            cj.angularXMotion = ConfigurableJointMotion.Locked;
+            cj.angularYMotion = ConfigurableJointMotion.Locked;
+            cj.angularZMotion = ConfigurableJointMotion.Locked;
         }
     }
 }
